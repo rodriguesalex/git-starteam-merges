f8204380599104a51edb93ccfb3356d7a7e06f2c
Patrick Higgins <patrick.allen.higgins@gmail.com>
2014-03-13 22:50:30 -0600

    Use gsm-add-merges in test.sh.

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $(dirname $0)
+
 # Import the test repos.
 # repo1 is the reference repo with merges.
 # repo2 is the repo without merges that should be reconstructed.
@@ -9,14 +11,29 @@ for repo in repo1 repo2 ; do
     ( cd $repo.git && git fast-import < ../$repo.export )
 done
 
+# Convert the tags in repo2 to annotated tags.
+(
+    cd repo2.git
+    git for-each-ref refs/tags |
+    while read obj objtype ref ; do
+        tag=${ref#refs/tags/}
+        git tag -d "$tag"
+        git tag -a -f -m "$tag" "$tag" "$obj"
+    done
+)
+
+# Build gsm-add-merges
+(
+    cd ../gsm-add-merges
+    go build
+)
+
 # Add the merges to repo2.
 (
     cd repo2.git
-    git filter-branch -f \
-        --parent-filter "gsm-parent-filter -in $PWD/../repo2-merges.csv" \
-        --tag-name-filter cat \
-        -- --all
-    rm -rf refs/original
+    git fast-export --no-data --all | \
+        ../../gsm-add-merges/gsm-add-merges -in ../repo2-merges.csv | \
+        git fast-import --force
 )
 
 # Check that the number of revisions are equal
